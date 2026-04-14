1b4cbcfc09bd38c2dea25b4fc881961a1ffce6c7
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-04-14 11:01:39 -0300

    :heavy_plus_sign: plus: html

--- Projetos/VhBurguer/vh-burguer/src/pages/login/index.tsx
+++ Projetos/VhBurguer/vh-burguer/src/pages/login/index.tsx
@@ -4,6 +4,22 @@
 const Login = () => {
     return (
         <>
+            <main>
+                <img src="" alt="" />
+                <aside>
+                    <h1>Login</h1>
+                    <form action="">
+                        <label htmlFor="email">E-mail</label>
+                        <input type="mail" placeholder="email@exemplo.com" name="email" required />
+
+                        <label htmlFor="password">Senha</label>
+                        <input type="password" placeholder="********" name="password" required />
+                        <a href="">Esqueceu sua senha?</a>
+
+                        <button type="submit" name="entrar" className="btn-entrar">Entrar</button>
+                    </form>
+                </aside>
+            </main>
         </>
     );
 }

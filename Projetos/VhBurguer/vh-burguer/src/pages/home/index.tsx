cd0eea71f1e5bdab303f26cdb579b6431dc7e07b
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-04-17 10:56:52 -0300

    :sparkles: feat: structure html of the home page

--- Projetos/VhBurguer/vh-burguer/src/pages/home/index.tsx
+++ Projetos/VhBurguer/vh-burguer/src/pages/home/index.tsx
@@ -1,16 +1,68 @@
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/Header";
+import styles from "../home/home.module.css"
 
 const Home = () => {
     return (
         <>
             <Header />
             <main>
-                <section></section>
-                <section></section>
-                <section></section>
-                <section></section>
-            </main>
+                <section id={styles.banner}>
+                    <div id={styles.conteiner_banner}>
+                        <p id={styles.titulo}>BEM-VINDOS AO VH BURGUER</p>
+                        <img src="../imgs/foto_de_hamburgueres.png" alt="Foto que contem tres bonitos hamburgueres." id={styles.banner_hamb_img} />
+                        <div id={styles.buttons_header}>
+                            <button id={styles.btn_chamar_atendente}>
+                                Chamar atendente
+                            </button>
+
+                            <button id={styles.btn_ver_cardapio}>
+                                Ver cardapio
+                            </button>
+                        </div>
+                    </div>
+                </section>
+
+                <section id={styles.destaques}>
+                    <div id={styles.conteiner_destaques}>
+                        <div className={styles.dest_grande}>
+                            <p className={styles.dest_text}>Os queridinhos da galera</p>
+                            <h3 className={styles.text_enfase}>MAIS PEDIDOS</h3>
+                        </div>
+
+                        <div className={styles.lado_destaque}>
+                            <div className={styles.dest_peq}>
+                                <p className={styles.dest_text}>Lanches com</p>
+                                <h3>MUITO BACON</h3>
+                            </div>
+
+                            <div className={styles.dest_peq}>
+                                <p className={styles.dest_text}>Se tiver muita fome</p>
+                                <h3>SUPER COMBOS</h3>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id={styles.cardapio}>
+                    <h2>Cardapio</h2>
+                    {/* Chamar componente da lista */}
+                </section>
+                <section id={styles.unidades}>
+                    <div id={styles.unidades_img}>
+                        <img src="../imgs/unidade.jpeg" alt="Imagem de uma da area interna de um restaurante de uma das unidade do VH Burguer" />
+                        <div id={styles.side_unidades}>
+                            <h3>Nossar uniddade</h3>
+                            <li id={styles.unidades_lista}>
+                                <ul>Centro - Av. Aurora, 742</ul>
+                                <ul>Jardim - Av. das Palmeiras, 1280</ul>
+                                <ul>Norte - Av. Horizonte, 305</ul>
+                                <ul>Sul - Av. Nova esperança, 910</ul>
+                            </li>
+                        </div>
+                    </div>
+                </section>
+            </main >
             <Footer />
         </>
     );

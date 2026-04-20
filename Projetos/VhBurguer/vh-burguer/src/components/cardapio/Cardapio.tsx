2b487f3213107aa7f75400a2536982e2a9bec98f
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-04-20 14:10:42 -0300

    :spakles: feat: add new component: cardapio

---
+++ Projetos/VhBurguer/vh-burguer/src/components/cardapio/Cardapio.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import styles from '../cardapio/Cardapio.module.css'
+
+const Cardapio = () => {
+    return (
+        <section id={styles.cardapio}>
+            <h2 id={styles.titulo}>CARDÁPIO</h2>
+            <div id={styles.conteiner}>
+                <button id={styles.btn_filtro}>
+                    <span className={styles.texto_btn_filtro}>Filtrar</span>
+                    <img src="../imgs/vector.svg" alt="" />
+            </button>
+            <div id={styles.content}>
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+
+                <article className={styles.card}>
+                    <img src="../imgs/hamburguer_alcatra.png" alt="Um lindo hamburguer, sobre uma tabua de madeira e uma faca do lado." className={styles.img_card} />
+                    <div className={styles.nome_produto}>
+                        <h3 className={styles.titulo_card}>Monster</h3>
+                    </div>
+                    <div className={styles.desc_card_content}>
+                        <p className={styles.desc_card}>Hámburguer brutal, suculento e exageradamente saboroso</p>
+                    </div>
+                    <div className={styles.preco_content}>
+                        <span className={styles.preco}>R$: 35,00</span>
+                    </div>
+                </article>
+            </div>
+        </div>
+        </section >
+    )
+}
+
+export default Cardapio
